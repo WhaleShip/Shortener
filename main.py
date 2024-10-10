19c86d1d301a348538ea0bb572e6424f960c64c6
WhaleShip <andreipogirei@gmail.com>
2024-10-10 19:47:30 +0500

    project init

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+
+from fastapi import FastAPI
+from uvicorn import run
+
+def get_app() -> FastAPI:
+    application = FastAPI(
+        title="Shortener",
+        docs_url="/swagger",
+    )
+    return application
+
+
+app = get_app()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    run(
+        "main:app",
+        host="0.0.0.0",
+        port=8080,
+        reload=True,
+        reload_dirs=[os.path.basename(os.path.dirname(__file__))],
+        log_level="debug",
+    )
